6ac71a9a44e4a76ea9e186d8e326076f03b08bd7
MaddnS <martin.schweighofer94@gmx.at>
2022-11-13 15:58:15 +0100

    implementing Keras Model

--- ai_picture_classification/src/app/takePicture/takePicture.page.ts
+++ ai_picture_classification/src/app/takePicture/takePicture.page.ts
@@ -4,9 +4,9 @@ import { ModalComponent } from '../modal/modal.component';
 import { PhotoService } from '../services/photo.service';
 import { Photo } from '@capacitor/camera';
 import { Prediction } from '../types/prediction';
-import * as tf from '@tensorflow/tfjs';
-import * as mobilenet from '@tensorflow-models/mobilenet';
+//import * as mobilenet from '@tensorflow-models/mobilenet';
 import { Platform } from '@ionic/angular';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-takePicture',
@@ -18,9 +18,10 @@ export class TakePicturePage implements OnInit {
   model: any;
   loading: boolean;
   imageSrc: string;
-  predictions: Prediction[];
-
+  predictions: Prediction[] | any;
   @ViewChild('img') imageEl: ElementRef;
+  modelKeras: tf.LayersModel;
+
   constructor(
     private photoService: PhotoService,
     private modalCtrl: ModalController,
@@ -28,11 +29,11 @@ export class TakePicturePage implements OnInit {
   ) {}
 
   async ngOnInit() {
-    const modelUrl =
-      'https://storage.googleapis.com/tfjs-models/savedmodel/mobilenet_v2_1.0_224/model.json';
+    //const modelUrl = '../model/model.json';
+    const modelUrl = 'http://localhost:42069/lichen_model';
     this.loading = true;
-    this.model = await mobilenet.load();
-    //this.model = await tf.loadGraphModel(modelUrl);
+    //this.model = await mobilenet.load();
+    this.modelKeras = await tf.loadLayersModel(modelUrl);
     this.loading = false;
   }
 
@@ -45,8 +46,11 @@ export class TakePicturePage implements OnInit {
       reader.onload = (res: any) => {
         this.imageSrc = res.target.result;
         setTimeout(async () => {
-          const imgEl = this.imageEl.nativeElement;
-          this.predictions = await this.model.classify(imgEl);
+          const imgEl = await tf.browser.fromPixels(this.imageEl.nativeElement);
+          //this.predictions = await this.model.classify(imgEl);
+          this.predictions = await this.modelKeras.predict([imgEl], {
+            batchSize: 1,
+          });
         }, 0);
       };
     }
@@ -55,7 +59,7 @@ export class TakePicturePage implements OnInit {
   classifyCamPic() {
     this.photoService.makePhotoForAI().then((data) => {
       this.imageSrc = data.base64;
-      const myImage = new Image(1024, 1024);
+      const myImage = new Image(256, 256);
 
       if (this.platform.is('hybrid')) {
         myImage.src = '	data:image/jpeg;base64,' + data.base64;
@@ -66,7 +70,9 @@ export class TakePicturePage implements OnInit {
       }
 
       setTimeout(async () => {
-        this.predictions = await this.model.classify(myImage);
+        //this.predictions = await this.model.classify(myImage);
+        const imgForClassification = tf.browser.fromPixels(myImage);
+        this.predictions = await this.modelKeras.predict(imgForClassification);
       }, 0);
     });
   }
